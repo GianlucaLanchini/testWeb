815403bb5ff43e51e1d8c2b2c852b2aec6e12c83
GianlucaLanchini <gianlucalanchini@gmail.com>
2021-10-25 23:13:31 +0200

    added html

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.get('/', (req, res) => {
   res.send('Ayo the pizza here!')
 })
 
+app.get('/prodotto/:codiceprodotto', (req, res) => {
+  
+  // andare a cercare il prodotto sul db
+  // generazione pagina html
+  var paginaWebProdotto = "<html><body>";
+  paginaWebProdotto+="<div>" + req.params.codiceprodotto + "</div>";
+  paginaWebProdotto+="</body></html>"
+  res.send(paginaWebProdotto)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
